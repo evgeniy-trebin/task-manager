22557e3bbd727131c876fae8fd909cb9ffa03476
evgeniy-trebin <evgeniy.trebin@gmail.com>
2016-01-30 13:51:38 +0300

    seeds with User data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'factory_girl_rails'
+require 'faker'
+User.create(email: 'test@ya.ru', password: '123123', password_confirmation: '123123')
+FactoryGirl::create_list(:user, 10)
+User.all.each do |user|
+  FactoryGirl::create_list(:task, rand(3), user: user)
+end
